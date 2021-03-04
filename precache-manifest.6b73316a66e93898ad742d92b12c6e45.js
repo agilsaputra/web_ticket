d36a8b5cc69e83ce8864c63d53ecb52d3c55a7ca
agilsaputra <m.agil.saputra@gmail.com>
2021-03-04 21:55:18 +0700

    Updates

---
+++ precache-manifest.6b73316a66e93898ad742d92b12c6e45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a162b3b85bd7f2876abcebb61df5c2b9",
+    "url": "/web_ticket/index.html"
+  },
+  {
+    "revision": "e792f30cb184254b88a2",
+    "url": "/web_ticket/static/css/main.9e0c16e3.chunk.css"
+  },
+  {
+    "revision": "40ab88f0ffdc8816254b",
+    "url": "/web_ticket/static/js/2.df705d98.chunk.js"
+  },
+  {
+    "revision": "26c31c05639d8c8e4d046eb1b98016c3",
+    "url": "/web_ticket/static/js/2.df705d98.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e792f30cb184254b88a2",
+    "url": "/web_ticket/static/js/main.98a7e91d.chunk.js"
+  },
+  {
+    "revision": "f00c31fdd4f3e1bbda4a",
+    "url": "/web_ticket/static/js/runtime-main.f567766d.js"
+  }
+]);
